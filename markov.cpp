e52946ba206c9dc7ebefa99ff3b801c89e0cc873
NathanEpstein <ne2210@columbia.edu>
2015-01-21 09:15:17 -0500

    fixed GBM bug

--- markov.cpp
+++ markov.cpp
@@ -2,38 +2,36 @@
 #include <vector>
 #include <random>
 
-using namespace std;
-
 //sorting function, to sort arrival times in poissP
 bool sortAsc(double i,double j){
   return (i<j);
 };
 
 //Markov simulation functions
-vector<double> poissP(double lambda, double T){
-  random_device rd;
-  mt19937 gen(rd());
-  poisson_distribution<> P(lambda*T);
+std::vector<double>poissP(double lambda, double T){
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::poisson_distribution<> P(lambda*T);
 
   //generate number of arrivals (n), arrival times are ~U(0,T) (given n).
   int n = P(gen);
-  vector<double> myVec(n);
+  std::vector<double> myVec(n);
   for (int i=0; i<n; i++){
-    myVec[i] = generate_canonical<double,10>(gen)*T;
+    myVec[i] = std::generate_canonical<double,10>(gen)*T;
   };
   //sort the arrival times and return the vector
   sort(myVec.begin(),myVec.end(),sortAsc);
   return myVec;
 };
 
-vector<double> brownian(double mu, double sigma, double T, int steps){
+std::vector<double> brownian(double mu, double sigma, double T, int steps){
   double dt = T/steps;
-  vector<double> myVec(steps+1);
+  std::vector<double> myVec(steps+1);
 
   //generator for determining dB in each increment
-  random_device rd;
-  mt19937 gen(rd());
-  normal_distribution<>norm(mu*dt,sigma*sqrt(dt));
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::normal_distribution<>norm(mu*dt,sigma*sqrt(dt));
 
   //generate BM, step by step
   myVec[0] = 0;
@@ -43,9 +41,9 @@ vector<double> brownian(double mu, double sigma, double T, int steps){
   return myVec;
 };
 
-vector<double> geoBrownian(double S0,double mu,double sigma, double T, int steps){
+std::vector<double> geoBrownian(double S0,double mu,double sigma, double T, int steps){
   // generate BM vector
-  vector<double> myVec = brownian(mu,sigma,T,steps);
+  std::vector<double> myVec = brownian(mu - (pow(sigma,2)/2),sigma,T,steps);
   // and map each element
   for(int i=0; i<myVec.size();i++){
     myVec[i] = S0*exp(myVec[i]);
@@ -53,12 +51,12 @@ vector<double> geoBrownian(double S0,double mu,double sigma, double T, int steps
   return myVec;
 };
 
-vector<int> DTMC (vector< vector<double> > trans, int steps, int start){
+std::vector<int> DTMC (std::vector< std::vector<double> > trans, int steps, int start){
   //random generator
-  random_device rd;
-  mt19937 gen(rd());
+  std::random_device rd;
+  std::mt19937 gen(rd());
   //initialize state vector
-  vector<int> myVec(steps+1);
+  std::vector<int> myVec(steps+1);
   myVec[0] = start;
   //initialize counter variables
   int count;
@@ -69,7 +67,7 @@ vector<int> DTMC (vector< vector<double> > trans, int steps, int start){
   for(int i=0; i<steps; i++){
     count = 0;
     sum = 0;
-    U = generate_canonical<double,10>(gen);
+    U = std::generate_canonical<double,10>(gen);
     while(sum < U){
       sum += trans[myVec[i]][count];
       if(sum > U){
@@ -81,15 +79,15 @@ vector<int> DTMC (vector< vector<double> > trans, int steps, int start){
   return myVec;
 };
 
-vector< vector<double> > CTMC(vector< vector<double> > trans, double T, int start){
+std::vector< std::vector<double> > CTMC(std::vector< std::vector<double> > trans, double T, int start){
   //random generator
-  random_device rd;
-  mt19937 gen(rd());
+  std::random_device rd;
+  std::mt19937 gen(rd());
   // results vector
-  vector< vector<double> > myVec;
+  std::vector< std::vector<double> > myVec;
 
   //include the beggining of the CTMC in the results
-  vector<double> myStep;
+  std::vector<double> myStep;
   myStep.push_back(0); //start time
   myStep.push_back(start); //start state
   myVec.push_back(myStep); //initialize results vector
@@ -109,13 +107,13 @@ vector< vector<double> > CTMC(vector< vector<double> > trans, double T, int star
     for (int i=0; i<trans[state].size();i++){
       lambda += trans[state][i];
     };
-    exponential_distribution<> stepT(lambda);
+    std::exponential_distribution<> stepT(lambda);
     t += stepT(gen);
     if (t < T){
       //determine new state
       j = 0;
       sum = 0;
-      U = generate_canonical<double,10>(gen);
+      U = std::generate_canonical<double,10>(gen);
       while(sum < U){
         sum += trans[state][j]/lambda;
         if (sum > U){
