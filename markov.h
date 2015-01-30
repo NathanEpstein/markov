090131dd2594d24fd978e58d1c307cdf0596470d
NathanEpstein <ne2210@columbia.edu>
2015-01-30 12:49:52 -0500

    modify header

--- markov.h
+++ markov.h
@@ -12,4 +12,15 @@ std::vector<double> geoBrownian(double,double,double, double, int);
 
 std::vector<int> DTMC (std::vector< std::vector<double> >, int, int);
 
-// std::vector< std::vector<double> > CTMC(std::vector< std::vector<double> >, double, int);
+class CTMC{
+public:
+  CTMC(std::vector< std::vector<double> > initMatrix);
+  ~CTMC();
+  void simulate(double T, int state);
+  std::vector<int> getStates();
+  std::vector<double> transTimes();
+protected:
+  std::vector< std::vector<double> > matrix;
+  std::vector<int> states;
+  std::vector<double> times;
+};
